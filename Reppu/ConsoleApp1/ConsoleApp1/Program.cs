2101fccee60f95eee11364f3fbd7497438bba15e
noahbergman <116635096+noahbergman@users.noreply.github.com>
2024-04-23 08:45:01 +0300

    .
    .

---
+++ Reppu/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿public class tavara
+{
+    protected double paino;
+    protected double tilavuus;
+
+    public double Paino
+    {
+        get { return paino; }
+        set { paino = value; }
+    }
+
+    public double Tilavuus
+    {
+        get { return tilavuus; }
+        set {  tilavuus = value; }
+    }
+
+    public tavara(double paino, double tilavuus)
+    {
+        this.paino = paino;
+        this.tilavuus = tilavuus;
+    }
+}
+
+public class reppu
+{
+    private double maxPaino;
+    private double maxTilavuus;
+    private tavara[] sisältö;
+
+    public reppu(double maxPaino, double maxTilavuus)
+    {
+        this.maxPaino = maxPaino;
+        this.maxTilavuus = maxTilavuus;
+    }
+}
+
+public class nuoli : tavara
+{
+    public nuoli(double paino, double tilavuus) : base(0.1, 0.05) { }
+}
+
+public class jousi : tavara
+{
+    public jousi(double paino, double tilavuus) : base(1, 4) { }
+}
+
+public class köysi : tavara
+{
+    public köysi(double paino, double tilavuus) : base(1, 1.5) { }
+}
+
+public class vesi : tavara
+{
+    public vesi(double paino, double tilavuus) : base(2, 2) { }
+}
+
+public class ruokaannos : tavara
+{
+    public ruokaannos(double paino, double tilavuus) : base(1, 0.5) { }
+}
+
+public class miekka : tavara
+{
+    public miekka(double paino, double tilavuus) : base(5, 3) { }
+}
---
+++ Ruoka/ruoka/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ruoka
+{
+    internal class Program
+    {
+        static void tilaus()
+        {
+            Console.WriteLine("Kokoa annoksesi: ");
+
+            Console.Write("Pääraaka-aine  (nautaa, kanaa, kasviksia): ");
+            string vastaus = Console.ReadLine();
+
+            Console.Write("Lisuke  (riisiä, perunaa, pastaa): ");
+            string vastaus2 = Console.ReadLine();
+
+            Console.Write("Kastike  (curry, pippuri, hapanimelä, chili): ");
+            string vastaus3 = Console.ReadLine();
+        }
+
+        enum paa {nautaa, kanaa, kasviksia }
+        enum lisuke {riisiä, perunaa, pastaa}
+        enum kastike {curry, pippuri, hapanimela, chili}
+    }
+}
