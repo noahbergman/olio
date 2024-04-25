d30595f5dc783ec29742a617339bc3d9d74dcb24
noahwiz <s2100947@edu.bc.fi>
2024-04-25 12:05:29 +0300

    4.0
    reppu eka tehtävä

--- Reppu/ConsoleApp1/ConsoleApp1/Program.cs
+++ Reppu/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,66 +1,159 @@
-﻿public class tavara
+﻿namespace sigma
 {
-    protected double paino;
-    protected double tilavuus;
-
-    public double Paino
+    internal class Program
     {
-        get { return paino; }
-        set { paino = value; }
+        static void Main(string[] args)
+        {
+            reppu lol = new reppu(30, 20, 10);
+            lol.yeeah();
+        }
+
+        public class tavara
+        {
+            protected double paino;
+            protected double tilavuus;
+
+            public double Paino
+            {
+                get { return paino; }
+                set { paino = value; }
+            }
+
+            public double Tilavuus
+            {
+                get { return tilavuus; }
+                set { tilavuus = value; }
+            }
+
+            public tavara(double paino, double tilavuus)
+            {
+                this.paino = paino;
+                this.tilavuus = tilavuus;
+            }
+        }
+
+        public class reppu
+        {
+            private double maxPaino;
+            private double maxTilavuus;
+            private int maxTavarat;
+            private double currentPaino = 0;
+            private double currentTilavuus = 0;
+            private int currentTavarat = 0;
+            private tavara[] sisältö;
+
+            static nuoli Nuoli = new nuoli();
+            static jousi Jousi = new jousi();
+            static köysi Köysi = new köysi();
+            static vesi Vesi = new vesi();
+            static ruokaannos Ruokaannos = new ruokaannos();
+            static miekka Miekka = new miekka();
+            private tavara[] reff = { Nuoli, Jousi, Köysi, Vesi, Ruokaannos, Miekka };
+
+            public reppu(double maxPaino, double maxTilavuus, int maxTavarat)
+            {
+                this.maxPaino = maxPaino;
+                this.maxTilavuus = maxTilavuus;
+                this.maxTavarat = maxTavarat;
+
+                sisältö = new tavara[maxTavarat];
+            }
+
+            public void yeeah()
+            {
+                while (currentTavarat < maxTavarat)
+                {
+                    Console.WriteLine($"Repussa on tällä hetkellä {currentTavarat}/{maxTavarat} tavaraa, {currentPaino}/{maxPaino} painoa, ja {currentTilavuus}/{maxTilavuus} tilaa");
+                    Console.WriteLine("Mitä haluat lisätä?");
+                    Console.WriteLine("1. Nuoli");
+                    Console.WriteLine("2. Jousi");
+                    Console.WriteLine("3. Köysi");
+                    Console.WriteLine("4. Vettä");
+                    Console.WriteLine("5. Ruokaa");
+                    Console.WriteLine("6. Miekka");
+
+                    while (true)
+                    {
+                        string vastaus = Console.ReadLine();
+                        bool check = false;
+                        int x;
+
+                        if (Int32.TryParse(vastaus, out x))
+                        {
+                            for (int i = 0; i < x; i++)
+                            {
+                                if (i == x - 1)
+                                {
+                                    bool kek = lisää(reff[i]);
+
+                                    if (kek == false)
+                                    {
+                                        Console.WriteLine("Esine ei mahtunut");
+                                    }
+
+                                    check = true;
+                                }
+                            }
+                        }
+
+                        if (check)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine("Valitse annetuista vaihtoehdoista, kirjoita pelkkä numero");
+                    }
+                }
+            }
+
+            bool lisää(tavara tavara)
+            {
+                if (currentPaino + tavara.Paino < maxPaino && currentTilavuus + tavara.Tilavuus < maxTilavuus)
+                {
+                    sisältö = sisältö.Append(tavara).ToArray();
+
+                    currentPaino += tavara.Paino;
+                    currentTilavuus += tavara.Tilavuus;
+                    currentTavarat++;
+
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public class nuoli : tavara
+        {
+            public nuoli() : base(0.1, 0.05) { }
+        }
+
+        public class jousi : tavara
+        {
+            public jousi() : base(1, 4) { }
+        }
+
+        public class köysi : tavara
+        {
+            public köysi() : base(1, 1.5) { }
+        }
+
+        public class vesi : tavara
+        {
+            public vesi() : base(2, 2) { }
+        }
+
+        public class ruokaannos : tavara
+        {
+            public ruokaannos() : base(1, 0.5) { }
+        }
+
+        public class miekka : tavara
+        {
+            public miekka() : base(5, 3) { }
+        }
     }
-
-    public double Tilavuus
-    {
-        get { return tilavuus; }
-        set {  tilavuus = value; }
-    }
-
-    public tavara(double paino, double tilavuus)
-    {
-        this.paino = paino;
-        this.tilavuus = tilavuus;
-    }
-}
-
-public class reppu
-{
-    private double maxPaino;
-    private double maxTilavuus;
-    private tavara[] sisältö;
-
-    public reppu(double maxPaino, double maxTilavuus)
-    {
-        this.maxPaino = maxPaino;
-        this.maxTilavuus = maxTilavuus;
-    }
-}
-
-public class nuoli : tavara
-{
-    public nuoli(double paino, double tilavuus) : base(0.1, 0.05) { }
-}
-
-public class jousi : tavara
-{
-    public jousi(double paino, double tilavuus) : base(1, 4) { }
-}
-
-public class köysi : tavara
-{
-    public köysi(double paino, double tilavuus) : base(1, 1.5) { }
-}
-
-public class vesi : tavara
-{
-    public vesi(double paino, double tilavuus) : base(2, 2) { }
-}
-
-public class ruokaannos : tavara
-{
-    public ruokaannos(double paino, double tilavuus) : base(1, 0.5) { }
-}
-
-public class miekka : tavara
-{
-    public miekka(double paino, double tilavuus) : base(5, 3) { }
 }
