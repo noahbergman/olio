b3ac03d29ae1520436ba562030320217b3e05922
noahwiz <s2100947@edu.bc.fi>
2024-05-10 13:43:46 +0300

    -
    -

---
+++ Harjoitustyö/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public class Pelaaja
+        {
+            protected int hp;
+            protected int str;
+
+            public int HP
+            {
+                get { return hp; }
+                set { hp = value; }
+            }
+
+            public int STR
+            {
+                get { return str; }
+                set { str = value; }
+            }
+
+        }
+
+        public class Vihollinen
+        {
+            protected int hp;
+            protected int str;
+
+            public int HP
+            {
+                get { return hp; }
+                set { hp = value; }
+            }
+
+            public int STR
+            {
+                get { return str; }
+                set { str = value; }
+            }
+
+        }
+
+        public class Esine
+        {
+
+        }
+
+        public class Kauppa
+        {
+
+        }
+    }
+}
